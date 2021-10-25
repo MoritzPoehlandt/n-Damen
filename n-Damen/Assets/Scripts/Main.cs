bfd9feb93103ccd591eaf4b2bc290f1b01effe39
MoritzPoehlandt <moritz.poehlandt@gmx.de>
2021-10-25 14:41:56 +0200

    Outsurce Code in Update Methode in Main with Functions

--- n-Damen/Assets/Scripts/Main.cs
+++ n-Damen/Assets/Scripts/Main.cs
@@ -25,59 +25,92 @@ public class Main : MonoBehaviour
 
     // Update is called once per frame
 
-    void Update()
+    private void Simulation()
     {
         //pressing the space starts the simulation. When pressed again, stop
-        if (Input.GetKeyDown("space")){
-            if (isPlay==false){
-                isPlay=true;
+        if (Input.GetKeyDown("space"))
+        {
+            if (isPlay == false)
+            {
+                isPlay = true;
                 Debug.Log("Play Simulaton");
-            }else {
-                isPlay=false;
+            }
+            else
+            {
+                isPlay = false;
                 Debug.Log("Stop Simulaton");
-                }
-		}
+            }
+        }
+    }
+
+    private void nextMove()
+    {
+        //when pressing to the right 1 step of the algorithm forward
+        if (Input.GetKeyDown("right"))
+        {
+            isPlay = false;
+            if (backtraking.arrayListLogAlgoritm[iteration].set_or_remove == true)
+            {
+                backtraking.createQueen(backtraking.arrayListLogAlgoritm[iteration].x + "_" +
+                backtraking.arrayListLogAlgoritm[iteration].y
+                , new Vector3(backtraking.arrayListLogAlgoritm[iteration].x, 0, backtraking.arrayListLogAlgoritm[iteration].y));
+            }
+            else
+            {
+                backtraking.deleteQueen(backtraking.arrayListLogAlgoritm[iteration].x + "_" +
+                     backtraking.arrayListLogAlgoritm[iteration].y);
+            }
+            iteration++;
+        }
+
         //While the mod play, we take one step in front and again check if the value has changed
-        if (isPlay){
-                if (backtraking.arrayListLogAlgoritm.Count>iteration){
-                     if (backtraking.arrayListLogAlgoritm[iteration].set_or_remove==true){
-                    backtraking.createQueen(backtraking.arrayListLogAlgoritm[iteration].x+"_"+
+        if (isPlay)
+        {
+            if (backtraking.arrayListLogAlgoritm.Count > iteration)
+            {
+                if (backtraking.arrayListLogAlgoritm[iteration].set_or_remove == true)
+                {
+                    backtraking.createQueen(backtraking.arrayListLogAlgoritm[iteration].x + "_" +
                     backtraking.arrayListLogAlgoritm[iteration].y
-                    ,new Vector3(backtraking.arrayListLogAlgoritm[iteration].x,0,backtraking.arrayListLogAlgoritm[iteration].y));
-                    }else{
-                    backtraking.deleteQueen(backtraking.arrayListLogAlgoritm[iteration].x+"_"+
-                         backtraking.arrayListLogAlgoritm[iteration].y); 
-                    }
+                    , new Vector3(backtraking.arrayListLogAlgoritm[iteration].x, 0, backtraking.arrayListLogAlgoritm[iteration].y));
+                }
+                else
+                {
+                    backtraking.deleteQueen(backtraking.arrayListLogAlgoritm[iteration].x + "_" +
+                         backtraking.arrayListLogAlgoritm[iteration].y);
+                }
                 iteration++;
                 Thread.Sleep(1000);
             }
         }
+    }
+
+    private void previousMove()
+    {
         //when pressing to the left 1 step of the algorithm back 
-        if (Input.GetKeyDown("left")){
-            isPlay=false;
-            if(backtraking.arrayListLogAlgoritm[iteration-1].set_or_remove==true){
-                backtraking.deleteQueen(backtraking.arrayListLogAlgoritm[iteration-1].x+"_"+
-                         backtraking.arrayListLogAlgoritm[iteration-1].y);
-            }else
+        if (Input.GetKeyDown("left"))
+        {
+            isPlay = false;
+            if (backtraking.arrayListLogAlgoritm[iteration - 1].set_or_remove == true)
             {
-                backtraking.createQueen(backtraking.arrayListLogAlgoritm[iteration-1].x+"_"+
-                    backtraking.arrayListLogAlgoritm[iteration-1].y
-                    ,new Vector3(backtraking.arrayListLogAlgoritm[iteration-1].x,0,backtraking.arrayListLogAlgoritm[iteration].y));
-            }             
-        iteration--;   
-		}
-        //when pressing to the right 1 step of the algorithm forward
-		 if (Input.GetKeyDown("right")){
-             isPlay=false;
-             if (backtraking.arrayListLogAlgoritm[iteration].set_or_remove==true){
-                    backtraking.createQueen(backtraking.arrayListLogAlgoritm[iteration].x+"_"+
-                    backtraking.arrayListLogAlgoritm[iteration].y
-                    ,new Vector3(backtraking.arrayListLogAlgoritm[iteration].x,0,backtraking.arrayListLogAlgoritm[iteration].y));
-             }else{
-                    backtraking.deleteQueen(backtraking.arrayListLogAlgoritm[iteration].x+"_"+
-                         backtraking.arrayListLogAlgoritm[iteration].y); 
-             }
-            iteration++;
-		}
+                backtraking.deleteQueen(backtraking.arrayListLogAlgoritm[iteration - 1].x + "_" +
+                         backtraking.arrayListLogAlgoritm[iteration - 1].y);
+            }
+            else
+            {
+                backtraking.createQueen(backtraking.arrayListLogAlgoritm[iteration - 1].x + "_" +
+                    backtraking.arrayListLogAlgoritm[iteration - 1].y
+                    , new Vector3(backtraking.arrayListLogAlgoritm[iteration - 1].x, 0, backtraking.arrayListLogAlgoritm[iteration].y));
+            }
+            iteration--;
+        }
+    }
+
+    void Update()
+    {
+        Simulation();
+        nextMove();
+        previousMove();
+        
     }
 }
