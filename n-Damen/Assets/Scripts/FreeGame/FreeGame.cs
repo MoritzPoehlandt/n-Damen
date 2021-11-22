76ff2e66f719dc8d249d2b61bd09efb3d544b49b
Moritz Pöhlandt <moritz.poehlandt@gmx.de>
2021-11-22 12:25:04 +0100

    FreeGame different modes

--- n-Damen/Assets/Scripts/FreeGame/FreeGame.cs
+++ n-Damen/Assets/Scripts/FreeGame/FreeGame.cs
@@ -10,38 +10,6 @@ public class FreeGame : GameItems
 
     void Start()
     {
-        backtraking = new AlgorithmBT(5);
-        backtraking.createQueenWithClick = true;
-        backtraking.displayBoard();
-        
-        //backtraking.setQueenLogiс(0, 1);//add in arrayList
-        //!!!!der Code ist der gleiche, aber x und y sind vertauscht
-        //backtraking.simulation.setQueenSim(1, 0);//add in logik brett  
-        //backtraking.editBoard();// zeigt logik brett
-        //backtraking.nextStep();//use createQueen und  zeigt logik brett
-
-        backtraking.simulation.solveCount=0;
-        backtraking.simulation.solve(0,0);
-        Debug.Log("0 Step " +backtraking.simulation.solveCount);
-
-        //!!!!der Code ist der gleiche, aber x und y sind vertauscht
-        backtraking.simulation.setQueenSim(1, 0);
-        createQueen("0_1",new Vector3(0,0,1));
-        backtraking.simulation.solveCount=0;
-        backtraking.simulation.solve(0,0);
-        Debug.Log("1 Step " +backtraking.simulation.solveCount);
-
-
-        backtraking.simulation.setQueenSim(4, 1);
-        createQueen("1_4",new Vector3(1,0,4));
-        backtraking.simulation.solveCount=0;
-        backtraking.simulation.solve(0,0);
-        Debug.Log("2 Step " +backtraking.simulation.solveCount);
-
-        backtraking.editBoard();
-     
-
-        
         
     }
 
@@ -59,6 +27,113 @@ public class FreeGame : GameItems
         deleteQueen(x.ToString() + "_" + y.ToString());
         backtraking.simulation.solve(0, 0);
         backtraking.editBoard();
+    }
+
+    public void easy()
+    {
+        removeAllFromScene();
+        backtraking = new AlgorithmBT(5);
+        backtraking.createQueenWithClick = true;
+        backtraking.displayBoard();
+
+        backtraking.simulation.solveCount = 0;
+        backtraking.simulation.solve(0, 0);
+        Debug.Log("0 Step " + backtraking.simulation.solveCount);
+
+        //!!!!der Code ist der gleiche, aber x und y sind vertauscht
+        backtraking.simulation.setQueenSim(1, 0);
+        createQueen("0_1", new Vector3(0, 0, 1));
+        backtraking.simulation.solveCount = 0;
+        backtraking.simulation.solve(0, 0);
+        Debug.Log("1 Step " + backtraking.simulation.solveCount);
+
+
+        backtraking.simulation.setQueenSim(4, 1);
+        createQueen("1_4", new Vector3(1, 0, 4));
+        backtraking.simulation.solveCount = 0;
+        backtraking.simulation.solve(0, 0);
+        Debug.Log("2 Step " + backtraking.simulation.solveCount);
+
+        backtraking.editBoard();
+    }
+
+    public void medium()
+    {
+        removeAllFromScene();
+        backtraking = new AlgorithmBT(6);
+        backtraking.createQueenWithClick = true;
+        backtraking.displayBoard();
+
+        backtraking.simulation.solveCount = 0;
+        backtraking.simulation.solve(0, 0);
+        Debug.Log("0 Step " + backtraking.simulation.solveCount);
+
+        //!!!!der Code ist der gleiche, aber x und y sind vertauscht
+        backtraking.simulation.setQueenSim(2, 0);
+        createQueen("0_2", new Vector3(0, 0, 2));
+        backtraking.simulation.solveCount = 0;
+        backtraking.simulation.solve(0, 0);
+        Debug.Log("1 Step " + backtraking.simulation.solveCount);
+
+
+        backtraking.simulation.setQueenSim(5, 2);
+        createQueen("2_5", new Vector3(2, 0, 5));
+        backtraking.simulation.solveCount = 0;
+        backtraking.simulation.solve(0, 0);
+        Debug.Log("2 Step " + backtraking.simulation.solveCount);
+
+        backtraking.editBoard();
+    }
+
+    public void hard()
+    {
+        removeAllFromScene();
+        backtraking = new AlgorithmBT(7);
+        backtraking.createQueenWithClick = true;
+        backtraking.displayBoard();
+
+        backtraking.simulation.solveCount = 0;
+        backtraking.simulation.solve(0, 0);
+        Debug.Log("0 Step " + backtraking.simulation.solveCount);
+
+        //!!!!der Code ist der gleiche, aber x und y sind vertauscht
+        backtraking.simulation.setQueenSim(1, 0);
+        createQueen("0_1", new Vector3(0, 0, 1));
+        backtraking.simulation.solveCount = 0;
+        backtraking.simulation.solve(0, 0);
+        Debug.Log("1 Step " + backtraking.simulation.solveCount);
+
+
+        backtraking.simulation.setQueenSim(4, 1);
+        createQueen("1_4", new Vector3(1, 0, 4));
+        backtraking.simulation.solveCount = 0;
+        backtraking.simulation.solve(0, 0);
+        Debug.Log("2 Step " + backtraking.simulation.solveCount);
+
+        backtraking.editBoard();
+    }
+
+    private void removeAllFromScene()
+    {
+        if (GameObject.Find("board") != null)
+        {
+            Destroy(GameObject.Find("board"));
+        }
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                if (GameObject.Find(i + "_" + j) != null)
+                {
+                    Destroy(GameObject.Find(i + "_" + j));
+                }
+            }
+        }
+    }
+
+    public void help()
+    {
+        backtraking.nextStep();
     }
 
 
